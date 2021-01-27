1fead39eaf351e1b81d53ae6ed41ec4f0cba305c
Felipe Borges <lipex360@gmail.com>
2021-01-26 22:22:30 -0300

    Socket.io added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
 import * as S from './styles'
 
 import Header from '../../components/Header'
+
+const socket = io('http://localhost:3333')
 
 const Home = () => {
   const [message, setMessage] = useState('')
 
   useEffect(() => {
     setMessage('Boilerplate ReactJS')
+
+    // Socket Receive Message
+    socket.on('backend', (message: string) => {
+      console.log(message)
+
+      // Socket Emit Message
+      socket.emit('frontend', `Hello from frontend ${new Date().getTime()}`)
+    })
   }, [])
 
   return (
